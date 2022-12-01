49844c9939d188b646ea669fe0a6add2606993b5
Dominic-github <78993276+Dominic-github@users.noreply.github.com>
2022-12-01 21:51:21 +0700

    Add Model

---
+++ HRM/HRM/Model/Report/Report.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Model.Report
+{
+    class Report
+    {
+        public Report()
+        {
+
+        }
+        public string Title { get; set; }
+
+        public DateTime CreateAt { get; set; }
+
+        public string Text { get; set; }
+        public string Note { get; set; }
+
+    }
+
+
+}
