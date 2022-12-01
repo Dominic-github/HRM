78dbac06866d8fd0af41dd0aa6c42bee25758269
Dominic-github <78993276+Dominic-github@users.noreply.github.com>
2022-12-01 20:58:53 +0700

    Update Model

--- .github/README.md
+++ .github/README.md
@@ -1,1 +1,6 @@
 # HRM
+### This's my Project School
+
+# 
+
+
--- HRM/HRM/Databases/Database.cs
+++ HRM/HRM/Databases/Database.cs
@@ -53,11 +53,17 @@ namespace HRM.Databases
                 {
                     if (reader.Read())
                     {
-                        result[result.Length + 1] = String.Format("{0}", reader["username"]);
-                        result[result.Length + 1] = String.Format("{0}", reader["password"]);
-                        result[result.Length + 1] = String.Format("{0}", reader["firstName"]);
-                        result[result.Length + 1] = String.Format("{0}", reader["middleName"]);
-                        result[result.Length + 1] = String.Format("{0}", reader["lastName"]);
+                        result[result.Length + 1] = String.Format("{0}", reader["username"]);                // [1]
+                        result[result.Length + 1] = String.Format("{0}", reader["password"]);                // [2]
+                        result[result.Length + 1] = String.Format("{0}", reader["firstName"]);               // [3]
+                        result[result.Length + 1] = String.Format("{0}", reader["middleName"]);              // [4]
+                        result[result.Length + 1] = String.Format("{0}", reader["lastName"]);                // [5]
+                        result[result.Length + 1] = String.Format("{0}", reader["avata"]);                   // [6]
+                        result[result.Length + 1] = String.Format("{0}", reader["email"]);                   // [7]
+                        result[result.Length + 1] = String.Format("{0}", reader["phone"]);                   // [8]
+                        result[result.Length + 1] = String.Format("{0}", reader["dateOfBirth"]);             // [9]
+                        result[result.Length + 1] = String.Format("{0}", reader["address"]);                 // [10]
+                        result[result.Length + 1] = String.Format("{0}", reader["joinDate"]);                // [11]
                     }
 
                 }
