10d91d1ce91efbe78292e90ff32b6c48d6dbc846
Tan Hoang <78993276+Dominic-github@users.noreply.github.com>
2022-12-01 21:53:12 +0700

    Add Model (#15)

---
+++ HRM/HRM/Model/Company/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Model.Company
+{
+    class Company
+    {
+        public Company()
+        {
+
+        }
+        public string CompanyName { get; set; }
+
+        public string Address { get; set; }
+
+        public string TaxID { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string CreateAt { get; set; }
+
+
+    }
+}
