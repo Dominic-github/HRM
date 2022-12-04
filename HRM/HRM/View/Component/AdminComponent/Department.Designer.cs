e95e0601b087011072b9ea190ce9ae559351eddf
Tan Hoang <78993276+Dominic-github@users.noreply.github.com>
2022-12-05 00:57:05 +0700

    Update Software and Department (#25)

--- HRM/HRM/View/Component/AdminComponent/Department.Designer.cs
+++ HRM/HRM/View/Component/AdminComponent/Department.Designer.cs
@@ -32,12 +32,15 @@ namespace HRM.View.Component.AdminComponent
             this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
-            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.Dep_departmentName = new Guna.UI2.WinForms.Guna2TextBox();
-            this.Dep_save = new Guna.UI2.WinForms.Guna2Button();
-            this.Dep_clearText = new Guna.UI2.WinForms.Guna2Button();
             this.Dep_department = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.Dep_add = new Guna.UI2.WinForms.Guna2Button();
+            this.Dep_remove = new Guna.UI2.WinForms.Guna2Button();
+            this.Dep_departmentName = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.Dep_depMess = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.Dep_add_depNameHas = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.Dep_remove_depNameHas = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2Panel1.SuspendLayout();
             this.guna2Panel2.SuspendLayout();
             this.SuspendLayout();
@@ -66,9 +69,12 @@ namespace HRM.View.Component.AdminComponent
             // guna2Panel2
             // 
             this.guna2Panel2.AutoScroll = true;
+            this.guna2Panel2.Controls.Add(this.Dep_remove_depNameHas);
+            this.guna2Panel2.Controls.Add(this.Dep_depMess);
+            this.guna2Panel2.Controls.Add(this.Dep_add_depNameHas);
             this.guna2Panel2.Controls.Add(this.Dep_department);
-            this.guna2Panel2.Controls.Add(this.Dep_save);
-            this.guna2Panel2.Controls.Add(this.Dep_clearText);
+            this.guna2Panel2.Controls.Add(this.Dep_add);
+            this.guna2Panel2.Controls.Add(this.Dep_remove);
             this.guna2Panel2.Controls.Add(this.Dep_departmentName);
             this.guna2Panel2.Controls.Add(this.guna2HtmlLabel3);
             this.guna2Panel2.Controls.Add(this.guna2HtmlLabel2);
@@ -78,27 +84,73 @@ namespace HRM.View.Component.AdminComponent
             this.guna2Panel2.Size = new System.Drawing.Size(953, 445);
             this.guna2Panel2.TabIndex = 0;
             // 
-            // guna2HtmlLabel2
+            // Dep_department
             // 
-            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2HtmlLabel2.Location = new System.Drawing.Point(177, 218);
-            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
-            this.guna2HtmlLabel2.Size = new System.Drawing.Size(80, 20);
-            this.guna2HtmlLabel2.TabIndex = 0;
-            this.guna2HtmlLabel2.Text = "Department";
-            this.guna2HtmlLabel2.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Dep_department.BackColor = System.Drawing.Color.Transparent;
+            this.Dep_department.BorderColor = System.Drawing.Color.Gray;
+            this.Dep_department.BorderRadius = 10;
+            this.Dep_department.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.Dep_department.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Dep_department.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.Dep_department.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.Dep_department.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dep_department.ForeColor = System.Drawing.Color.Black;
+            this.Dep_department.ItemHeight = 30;
+            this.Dep_department.Items.AddRange(new object[] {
+            "--Select--"});
+            this.Dep_department.Location = new System.Drawing.Point(270, 213);
+            this.Dep_department.Name = "Dep_department";
+            this.Dep_department.Size = new System.Drawing.Size(391, 36);
+            this.Dep_department.TabIndex = 9;
             // 
-            // guna2HtmlLabel3
+            // Dep_add
             // 
-            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
-            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2HtmlLabel3.Location = new System.Drawing.Point(133, 87);
-            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
-            this.guna2HtmlLabel3.Size = new System.Drawing.Size(124, 20);
-            this.guna2HtmlLabel3.TabIndex = 0;
-            this.guna2HtmlLabel3.Text = "Department Name";
-            this.guna2HtmlLabel3.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Dep_add.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Dep_add.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(140)))), ((int)(((byte)(25)))));
+            this.Dep_add.BorderRadius = 20;
+            this.Dep_add.BorderThickness = 1;
+            this.Dep_add.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Dep_add.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Dep_add.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Dep_add.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Dep_add.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Dep_add.FillColor = System.Drawing.Color.White;
+            this.Dep_add.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dep_add.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(140)))), ((int)(((byte)(25)))));
+            this.Dep_add.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(140)))), ((int)(((byte)(25)))));
+            this.Dep_add.HoverState.ForeColor = System.Drawing.Color.White;
+            this.Dep_add.Location = new System.Drawing.Point(803, 369);
+            this.Dep_add.Margin = new System.Windows.Forms.Padding(12);
+            this.Dep_add.Name = "Dep_add";
+            this.Dep_add.Size = new System.Drawing.Size(105, 45);
+            this.Dep_add.TabIndex = 8;
+            this.Dep_add.Text = "Add";
+            this.Dep_add.Click += new System.EventHandler(this.Dep_add_Click);
+            // 
+            // Dep_remove
+            // 
+            this.Dep_remove.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Dep_remove.BackColor = System.Drawing.Color.Transparent;
+            this.Dep_remove.BorderColor = System.Drawing.Color.Red;
+            this.Dep_remove.BorderRadius = 20;
+            this.Dep_remove.BorderThickness = 1;
+            this.Dep_remove.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Dep_remove.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Dep_remove.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Dep_remove.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Dep_remove.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Dep_remove.FillColor = System.Drawing.Color.White;
+            this.Dep_remove.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dep_remove.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+            this.Dep_remove.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+            this.Dep_remove.HoverState.ForeColor = System.Drawing.Color.White;
+            this.Dep_remove.Location = new System.Drawing.Point(674, 369);
+            this.Dep_remove.Margin = new System.Windows.Forms.Padding(12);
+            this.Dep_remove.Name = "Dep_remove";
+            this.Dep_remove.Size = new System.Drawing.Size(105, 45);
+            this.Dep_remove.TabIndex = 7;
+            this.Dep_remove.Text = "Remove";
+            this.Dep_remove.Click += new System.EventHandler(this.Dep_remove_Click);
             // 
             // Dep_departmentName
             // 
@@ -123,69 +175,66 @@ namespace HRM.View.Component.AdminComponent
             this.Dep_departmentName.Size = new System.Drawing.Size(391, 36);
             this.Dep_departmentName.TabIndex = 1;
             // 
-            // Dep_save
+            // guna2HtmlLabel3
             // 
-            this.Dep_save.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.Dep_save.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(140)))), ((int)(((byte)(25)))));
-            this.Dep_save.BorderRadius = 20;
-            this.Dep_save.BorderThickness = 1;
-            this.Dep_save.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Dep_save.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.Dep_save.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.Dep_save.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.Dep_save.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.Dep_save.FillColor = System.Drawing.Color.White;
-            this.Dep_save.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Dep_save.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(140)))), ((int)(((byte)(25)))));
-            this.Dep_save.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(140)))), ((int)(((byte)(25)))));
-            this.Dep_save.HoverState.ForeColor = System.Drawing.Color.White;
-            this.Dep_save.Location = new System.Drawing.Point(803, 369);
-            this.Dep_save.Margin = new System.Windows.Forms.Padding(12);
-            this.Dep_save.Name = "Dep_save";
-            this.Dep_save.Size = new System.Drawing.Size(105, 45);
-            this.Dep_save.TabIndex = 8;
-            this.Dep_save.Text = "Save";
+            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(133, 87);
+            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(124, 20);
+            this.guna2HtmlLabel3.TabIndex = 0;
+            this.guna2HtmlLabel3.Text = "Department Name";
+            this.guna2HtmlLabel3.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // Dep_clearText
+            // guna2HtmlLabel2
             // 
-            this.Dep_clearText.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.Dep_clearText.BackColor = System.Drawing.Color.Transparent;
-            this.Dep_clearText.BorderColor = System.Drawing.Color.Red;
-            this.Dep_clearText.BorderRadius = 20;
-            this.Dep_clearText.BorderThickness = 1;
-            this.Dep_clearText.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Dep_clearText.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.Dep_clearText.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.Dep_clearText.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.Dep_clearText.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.Dep_clearText.FillColor = System.Drawing.Color.White;
-            this.Dep_clearText.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Dep_clearText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
-            this.Dep_clearText.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
-            this.Dep_clearText.HoverState.ForeColor = System.Drawing.Color.White;
-            this.Dep_clearText.Location = new System.Drawing.Point(674, 369);
-            this.Dep_clearText.Margin = new System.Windows.Forms.Padding(12);
-            this.Dep_clearText.Name = "Dep_clearText";
-            this.Dep_clearText.Size = new System.Drawing.Size(105, 45);
-            this.Dep_clearText.TabIndex = 7;
-            this.Dep_clearText.Text = "Remove";
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(177, 218);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(80, 20);
+            this.guna2HtmlLabel2.TabIndex = 0;
+            this.guna2HtmlLabel2.Text = "Department";
+            this.guna2HtmlLabel2.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // Dep_department
+            // Dep_depMess
             // 
-            this.Dep_department.BackColor = System.Drawing.Color.Transparent;
-            this.Dep_department.BorderColor = System.Drawing.Color.Gray;
-            this.Dep_department.BorderRadius = 10;
-            this.Dep_department.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.Dep_department.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.Dep_department.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.Dep_department.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.Dep_department.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Dep_department.ForeColor = System.Drawing.Color.Black;
-            this.Dep_department.ItemHeight = 30;
-            this.Dep_department.Location = new System.Drawing.Point(270, 213);
-            this.Dep_department.Name = "Dep_department";
-            this.Dep_department.Size = new System.Drawing.Size(391, 36);
-            this.Dep_department.TabIndex = 9;
+            this.Dep_depMess.BackColor = System.Drawing.Color.Transparent;
+            this.Dep_depMess.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.Dep_depMess.ForeColor = System.Drawing.Color.Red;
+            this.Dep_depMess.Location = new System.Drawing.Point(44, 361);
+            this.Dep_depMess.Name = "Dep_depMess";
+            this.Dep_depMess.Size = new System.Drawing.Size(201, 19);
+            this.Dep_depMess.TabIndex = 81;
+            this.Dep_depMess.TabStop = false;
+            this.Dep_depMess.Text = "• Department Name  is require !!!";
+            this.Dep_depMess.Visible = false;
+            // 
+            // Dep_add_depNameHas
+            // 
+            this.Dep_add_depNameHas.BackColor = System.Drawing.Color.Transparent;
+            this.Dep_add_depNameHas.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.Dep_add_depNameHas.ForeColor = System.Drawing.Color.Red;
+            this.Dep_add_depNameHas.Location = new System.Drawing.Point(44, 386);
+            this.Dep_add_depNameHas.Name = "Dep_add_depNameHas";
+            this.Dep_add_depNameHas.Size = new System.Drawing.Size(234, 19);
+            this.Dep_add_depNameHas.TabIndex = 82;
+            this.Dep_add_depNameHas.TabStop = false;
+            this.Dep_add_depNameHas.Text = "• Department Name  is already exist !!!";
+            this.Dep_add_depNameHas.Visible = false;
+            // 
+            // Dep_remove_depNameHas
+            // 
+            this.Dep_remove_depNameHas.BackColor = System.Drawing.Color.Transparent;
+            this.Dep_remove_depNameHas.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.Dep_remove_depNameHas.ForeColor = System.Drawing.Color.Red;
+            this.Dep_remove_depNameHas.Location = new System.Drawing.Point(44, 336);
+            this.Dep_remove_depNameHas.Name = "Dep_remove_depNameHas";
+            this.Dep_remove_depNameHas.Size = new System.Drawing.Size(197, 19);
+            this.Dep_remove_depNameHas.TabIndex = 83;
+            this.Dep_remove_depNameHas.TabStop = false;
+            this.Dep_remove_depNameHas.Text = "• Department Name  is invalid !!!";
+            this.Dep_remove_depNameHas.Visible = false;
             // 
             // Department
             // 
@@ -214,7 +263,10 @@ namespace HRM.View.Component.AdminComponent
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
         private Guna.UI2.WinForms.Guna2ComboBox Dep_department;
-        private Guna.UI2.WinForms.Guna2Button Dep_save;
-        private Guna.UI2.WinForms.Guna2Button Dep_clearText;
+        private Guna.UI2.WinForms.Guna2Button Dep_add;
+        private Guna.UI2.WinForms.Guna2Button Dep_remove;
+        private Guna.UI2.WinForms.Guna2HtmlLabel Dep_depMess;
+        private Guna.UI2.WinForms.Guna2HtmlLabel Dep_add_depNameHas;
+        private Guna.UI2.WinForms.Guna2HtmlLabel Dep_remove_depNameHas;
     }
 }
--- HRM/HRM/View/Component/AdminComponent/Department.cs
+++ HRM/HRM/View/Component/AdminComponent/Department.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HRM.Controller;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,93 @@ namespace HRM.View.Component.AdminComponent
         public Department()
         {
             InitializeComponent();
+            UpdateData();
+        }
+
+        public void UpdateData()
+        {
+            // Update Department
+
+            Dep_department.Items.Clear();
+            Dep_department.Items.Add("--Select--");
+            Dep_department.StartIndex = 0;
+            int index = 0;
+
+            while (index < C_Software.ListDep.Length)
+            {
+                Dep_department.Items.Add(C_Software.ListDep[index].DepartmentName);
+                index++;
+            }
+
+        }
+
+        public void ShowError(bool add)
+        {
+            if(Dep_departmentName.Text == "")
+            {
+                Dep_departmentName.BorderColor = Color.Red;
+                Dep_depMess.Visible = true;
+            }
+            else
+            {
+                Dep_departmentName.BorderColor = Color.Gray;
+                Dep_depMess.Visible = false;
+            }
+
+
+            int index = 0;
+
+            while (index < C_Software.ListDep.Length)
+            {
+                if (Dep_departmentName.Text == C_Software.ListDep[index].DepartmentName)
+                {
+                    if (add)
+                    {
+                        Dep_add_depNameHas.Visible = true;
+
+                    }
+                    else
+                    {
+                        Dep_remove_depNameHas.Visible = false;
+                    }
+                    break;
+                }
+                else
+                {
+                    if (add)
+                    {
+                        Dep_add_depNameHas.Visible = false;
+                    }
+                    else
+                    {
+                        Dep_remove_depNameHas.Visible = true;
+
+                    }
+                    index++;
+
+                }
+            }
+
+        }
+
+        private void Dep_add_Click(object sender, EventArgs e)
+        {
+            SoftwareAdmin sw = new SoftwareAdmin();
+            bool isAdd = sw.ShowAlterQuess(false);
+            if (isAdd)
+            {
+                ShowError(true);
+            }
+        }
+
+        private void Dep_remove_Click(object sender, EventArgs e)
+        {
+            SoftwareAdmin sw = new SoftwareAdmin();
+            bool isRemove = sw.ShowAlterQuess(false);
+            if (isRemove) {
+                ShowError(false);
+            }
+
         }
     }
 }
