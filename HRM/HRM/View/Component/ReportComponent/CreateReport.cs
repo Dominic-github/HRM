f9f713088c0783538b86b7d1b35e1478a93ff5ed
Mutteen <Nhuannghia1122@gmail.com>
2022-12-06 01:12:14 +0700

    add report

--- HRM/HRM/View/Component/ReportComponent/CreateReport.cs
+++ HRM/HRM/View/Component/ReportComponent/CreateReport.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HRM.Controller.Admin;
+using HRM.Controller;
+using HRM.Controller.Report;
+using HRM.View.Alter;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HRM.Controller.Component;
 
 namespace HRM.View.Component.ReportComponent
 {
@@ -15,6 +20,39 @@ namespace HRM.View.Component.ReportComponent
         public CreateReport()
         {
             InitializeComponent();
+        }
+
+        private void CreRp_btn_reset_Click(object sender, EventArgs e)
+        {
+            CreRp_tittle.Text = "";
+            CreRp_content.Text = "";
+        }
+
+        private void CreRp_btn_create_Click(object sender, EventArgs e)
+        {
+            string title= CreRp_tittle.Text;
+            title.Trim();
+            string content = CreRp_content.Text;
+            content.Trim();
+
+            SoftwareAdmin sw = new SoftwareAdmin();
+            bool isClick_Save = sw.ShowAlterQuess(false);
+
+            if (isClick_Save)
+            {
+                bool isDone = C_AddReport.AddReport(title, content);
+                if (isDone)
+                {
+                    Sucess sucess = new Sucess();
+                    sucess.ShowDialog();
+                    C_Software.UpdateDepartment();
+                }
+                else
+                {
+                    Error error = new Error();
+                    error.ShowDialog();
+                }
+            }
         }
     }
 }
