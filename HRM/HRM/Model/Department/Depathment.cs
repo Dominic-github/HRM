e1f6970cc71148f9a22347c5534678a8463ae558
Tan Hoang <78993276+Dominic-github@users.noreply.github.com>
2022-12-05 12:56:07 +0700

    Update Directory (#27)

--- HRM/HRM/Model/Department/Depathment.cs
+++ HRM/HRM/Model/Department/Depathment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HRM.Controller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,33 @@ using System.Threading.Tasks;
 
 namespace HRM.Model.Department
 {
+
     public class Department
     {
+        private static Department[] listDep = C_Software.ListDep;
+
         public int DepartmentID { get; set; }
 
         public string DepartmentName { get; set; }
 
         public int NumberOfEmployee { get; set; }
+
+        public static string GetDepartmentName(int DepartmentID)
+        {
+            string name = "";
+            int index = 0;
+            while(index < listDep.Length)
+            {
+                if(DepartmentID == listDep[index].DepartmentID)
+                {
+                    name = listDep[index].DepartmentName;
+                    break;
+
+                }
+                index++;
+            }
+            return name;
+        }
+
     }
 }
