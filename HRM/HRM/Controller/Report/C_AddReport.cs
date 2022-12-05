45c3636b4a3fbe444695216f0d2adcb7ee591348
Mutteen <92002400+Mutteen@users.noreply.github.com>
2022-12-06 02:25:44 +0700

    Add report (#31)
    * database nghia
    
    * add report
    
    * Delete Database.cs
    
    Not your path
    
    Co-authored-by: Tan Hoang <78993276+Dominic-github@users.noreply.github.com>

---
+++ HRM/HRM/Controller/Report/C_AddReport.cs
@@ -0,0 +1,22 @@
+﻿using HRM.Controller.Component;
+using HRM.Model.Employee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Controller.Report
+{
+    public class C_AddReport
+    {
+       
+
+        public static bool AddReport(string title, string content)
+        {
+
+            string queryString = $"insert into Report(emID, wordContent, jobDetail) values('{C_Software.Me.EmployeeID}','{title}','{content}')";
+            return C_Query.Add(queryString);
+        }
+    }
+}
